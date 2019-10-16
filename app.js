e1640be832bae1b3cee582d70d17fd47af5d779e
wengcheng <wengcheng@jianbing.com>
2019-10-16 15:38:12 +0800

    url

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-let launch = require('./routes/launch.js')
+var launch = require('./routes/launch')
 
 var app = express();
 
