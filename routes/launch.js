79529d8d5e028cef1b0c9e7b7a82a41ebcebf778
wengcheng <wengcheng@jianbing.com>
2019-10-16 16:25:16 +0800

    off

--- routes/launch.js
+++ routes/launch.js
@@ -2,10 +2,11 @@ var express = require('express');
 var router = express.Router();
 /* Post users listing. */
 router.post('/', function (req, res) {
-    res.end(JSON.stringify({ url: 'https://www.baidu.com/', launch: "launch" }))
+    // res.end(JSON.stringify({ url: 'https://www.baidu.com/', launch: "launch" }))
+    res.end(JSON.stringify({version:'1.0.0',language:'en'}))
 
 });
 router.get('/', function (req, res) {
-    res.end(JSON.stringify({ url: 'https://www.baidu.com/', launch: "launch" }))
+    // res.end(JSON.stringify({ url: 'https://www.baidu.com/', launch: "launch" }))
 });
 module.exports = router;
